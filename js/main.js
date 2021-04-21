156b707f621df9f47a66a6d3e5f62f2602225bde
Hasan alkholy <hassan.m.h.m.a123@gmail.com.com>
2021-04-21 13:37:38 +0200

    header

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var wow = new WOW();
+wow.init();
+
+// parallaxInstance.friction(0.2, 0.2);
+$(document).ready(function () {
+  $(".menu").click(function () {
+    $(".menu .menu-icon").removeClass("open-menu");
+    $(".menu .menu-icon").addClass("close-menu");
+    // $(".mo-nav").slideDown(500);
+    $(".mo-nav").addClass("menu-down");
+    $("body").addClass("overflow");
+  });
+  $(".close,.nav-li .nav-link").click(function () {
+    $(".menu .menu-icon").addClass("open-menu");
+    $(".menu .menu-icon").removeClass("close-menu");
+    // $(".mo-nav").slideUp(500);
+    $(".mo-nav").removeClass("menu-down");
+    $("body").removeClass("overflow");
+  });
+});
